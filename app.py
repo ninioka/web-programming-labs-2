839096b0965de6429746cfda53f2bde462c4f304
Нина Демченко <demchenko.nina2004@gmail.com>
2024-09-15 23:08:37 +0700

    Меню первой лабораторной

--- app.py
+++ app.py
@@ -119,3 +119,34 @@ def main():
     </body>
 </html>
 '''
+
+@app.route("/lab1")
+def lab1():
+    style = url_for("static", filename = "style.css")
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>Лабораторная 1</title>  
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных
+            <link rel="stylesheet" type="text/css" href="''' + style + '''">
+        </header>
+        <main>
+            <div>
+                Flask — фреймворк для создания веб-приложений на языке 
+                программирования Python, использующий набор инструментов 
+                Werkzeug, а также шаблонизатор Jinja2. Относится к категории так 
+                называемых микрофреймворков — минималистичных каркасов 
+                веб-приложений, сознательно предоставляющих лишь самые базовые возможности.<br>
+            </div>
+            <a href = '/'>Главная страница</a>
+        </main>
+        <footer>
+            &copy; Нина Демченко, ФБИ-22, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+'''
