c3f884c8b7e794e7c384ea80bd894372ba6fd57c
Нина Демченко <demchenko.nina2004@gmail.com>
2024-09-15 21:09:21 +0700

    Стили

--- app.py
+++ app.py
@@ -34,9 +34,13 @@ def author():
 @app.route('/lab1/oak')
 def oak():
     path = url_for("static", filename="oak.jpg")
+    path1 = url_for("static", filename = "lab1.css")
     return '''
 <!doctype html>
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="''' + path1 + '''">
+    </head>
     <body>
         <h1>Дуб</h1>
         <img src="''' + path + '''">
