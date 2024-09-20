a81028b56a546cde5e8b98bdd181d3a28288a0ed
Нина Демченко <demchenko.nina2004@gmail.com>
2024-09-20 21:36:02 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -495,4 +495,7 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Нина Демченко'
-    return render_template('example.html', name=name)
+    number = '2'
+    group = 'ФБИ-22'
+    course_number = '3'
+    return render_template('example.html', name=name, number=number, group=group, course_number=course_number)
