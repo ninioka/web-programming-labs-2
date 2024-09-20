8ce94e7051e08fa3419afecfb3c476eba79debb5
Нина Демченко <demchenko.nina2004@gmail.com>
2024-09-20 21:25:18 +0700

    Заготовка шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, render_template
 app = Flask(__name__)
 
 @app.route("/lab1/web")
@@ -491,3 +491,7 @@ def add_flower(name):
     </body>
 </html>
 '''
+
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
