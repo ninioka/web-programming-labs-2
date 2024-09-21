e9ca8ba12075abf191c7386c6dbfd023adf168d3
Нина Демченко <demchenko.nina2004@gmail.com>
2024-09-21 22:21:09 +0700

    Наследование шаблонов

--- app.py
+++ app.py
@@ -506,3 +506,7 @@ def example():
         {'name': 'Манго', 'price': 321}
         ]
     return render_template('example.html', name=name, lab_num=lab_num, group=group, course_number=course_number, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2():
+    return render_template('lab2.html')
