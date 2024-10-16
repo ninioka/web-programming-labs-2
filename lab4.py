8c7fb0eaf7995c43f9c5e90376f970d2296b5e88
Нина Демченко <demchenko.nina2004@gmail.com>
2024-10-16 22:52:55 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -26,3 +26,87 @@ def div():
 
     result = x1 / x2
     return render_template('/lab4/div.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('/lab4/sum_form.html')
+
+@lab4.route('/lab4/sum', methods = ['POST'])
+def sum():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' and x2 == '':
+        return render_template('/lab4/sum.html', error='Хотя бы одно поле должно быть заполнено!')
+    
+    if x1 == '':
+        x1 = 0
+    if x2 == '':
+        x2 = 0
+
+    x1 = int(x1)
+    x2 = int(x2)
+
+    result = x1 + x2
+    return render_template('/lab4/sum.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/multiply-form')
+def multiply_form():
+    return render_template('/lab4/multiply_form.html')
+
+@lab4.route('/lab4/multiply', methods = ['POST'])
+def multiply():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' and x2 == '':
+        return render_template('/lab4/multiply.html', error='Хотя бы одно поле должно быть заполнено!')
+    
+    if x1 == '':
+        x1 = 1
+    if x2 == '':
+        x2 = 1
+
+    x1 = int(x1)
+    x2 = int(x2)
+
+    result = x1 * x2
+    return render_template('/lab4/multiply.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('/lab4/sub_form.html')
+
+@lab4.route('/lab4/sub', methods = ['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if x1 == '' or x2 == '':
+        return render_template('/lab4/sub.html', error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+
+    result = x1 - x2
+    return render_template('/lab4/sub.html', x1=x1, x2=x2, result=result)
+
+
+@lab4.route('/lab4/deg-form')
+def deg_form():
+    return render_template('/lab4/deg_form.html')
+
+@lab4.route('/lab4/deg', methods = ['POST'])
+def deg():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+
+    if x1 == '' or x2 == '':
+        return render_template('/lab4/deg.html', error='Оба поля должны быть заполнены!')
+    
+    x1 = int(x1)
+    x2 = int(x2)
+
+    result = x1 ** x2
+    return render_template('/lab4/deg.html', x1=x1, x2=x2, result=result)
