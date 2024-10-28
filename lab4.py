cd299f9141a4fb8ffcc1b09ccb939ed91660c173
Нина Демченко <demchenko.nina2004@gmail.com>
2024-10-28 21:52:13 +0700

    -(2)

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, session, make_response
+from flask import Blueprint, render_template, request, redirect, session
 lab4 = Blueprint('lab4', __name__)
 
 
