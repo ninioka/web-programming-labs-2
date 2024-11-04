1045d4a149e158fd0742b64ef511bcaf490748f5
Нина Демченко <demchenko.nina2004@gmail.com>
2024-11-04 22:39:34 +0700

    Регистрация

--- lab5.py
+++ lab5.py
@@ -1,2 +1,43 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, session, redirect
+import psycopg2
 lab5 = Blueprint('lab5', __name__)
+
+@lab5.route('/lab5/')
+def lab():
+    name = request.cookies.get('name')
+
+    if name is None:
+        name = "Anonymous"
+    return render_template ('lab5/lab5.html', name=name)
+
+
+@lab5.route('/lab5/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('lab5/register.html')
+    
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab5/register.html', error='Заполните все поля!')
+    
+    conn = psycopg2.connect(
+        host = '127.0.0.1',
+        database = 'nina_demchenko_knowledge_base',
+        user = 'nina_demchenko_knowledge_base',
+        password = '123'
+    )
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    if cur.fetchone():
+        cur.close()
+        conn.close()
+        return render_template ('lab5/register.html', error="Такой пользователь уже существует")
+    
+    cur.execute(f"INSERT INTO users (login, password) VALUES ('{login}', '{password}')")
+    conn.commit()
+    cur.close()
+    conn.close()
+    return render_template('lab5/success.html')
