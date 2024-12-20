f8f22ba1e1626449acd4103ff68a886f59678778
Нина Демченко <demchenko.nina2004@gmail.com>
2024-12-20 15:03:32 +0700

    Заготовка к 9-ой лабораторной

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 
 app = Flask(__name__)
@@ -53,6 +54,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 
 @app.errorhandler(404)
@@ -108,6 +110,7 @@ def main():
                 <li><a href = "/lab6/">Шестая лабораторная</a></li>
                 <li><a href = "/lab7/">Седьмая лабораторная</a></li>
                 <li><a href = "/lab8/">Восьмая лабораторная</a></li>
+                <li><a href = "/lab9/">Девятая лабораторная</a></li>
             </ul>
         </main>
         <footer>
